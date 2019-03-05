b7e6e6cb9c7103bafcb66c416e84f2f098425f88
Arne Coucheron <arco68@gmail.com>
2019-03-05 01:10:44 +0100

    jfltevzw: Rewrite device tree and update for Lineage 16.0
    Change-Id: Ic4a85b7c5d0869f3ffadd37491328caa2762f4ea

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
-#
-# Copyright (C) 2011 The CyanogenMod Project
+# Copyright (C) 2013-2016 The CyanogenMod Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,27 +12,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-## (2) Also get non-open-source specific aspects if available
+# Inherit from jf-common
+$(call inherit-product, device/samsung/jf-common/jf-common.mk)
+
+# Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/jfltevzw/jfltevzw-vendor.mk)
 
-# loki
+# Device overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/jfltevzw/overlay
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml
+
+# Loki
 PRODUCT_PACKAGES += \
     loki_tool \
     loki.sh \
     loki_bootloaders \
     recovery-transform.sh
-
-PRODUCT_COPY_FILES += \
-    device/samsung/jfltevzw/releasetools/loki.sh:install/bin/loki.sh
-
-# Vendor security patch level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2017-01-05
-
-## device overlays
-DEVICE_PACKAGE_OVERLAYS += device/samsung/jfltevzw/overlay
-
-# Inherit from jf-common
-$(call inherit-product, device/samsung/jf-common/jf-common.mk)
