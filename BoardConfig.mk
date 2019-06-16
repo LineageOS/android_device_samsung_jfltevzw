87a63b157ab4e11b7b8680c0b04af0034dcb93d6
Arne Coucheron <arco68@gmail.com>
2019-06-16 06:59:15 +0000

    jfltevzw: manifest: Generate
    * Include Touch HAL.
    
    Change-Id: I882299102e4e8425733bd87858036909cd9872c8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2009 The CyanogenMod Project
+# Copyright (C) 2013-2016 The CyanogenMod Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,3 +27,6 @@ TARGET_KERNEL_VARIANT_CONFIG := jf_vzw_defconfig
 
 # Loki
 TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/jfltevzw/releasetools
+
+# Manifest
+DEVICE_MANIFEST_FILE += device/samsung/jfltevzw/manifest.xml
