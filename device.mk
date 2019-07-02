c34c95b3186a8e064d38a0526d0f34c17f1afbc0
Arne Coucheron <arco68@gmail.com>
2019-07-03 01:27:08 +0200

    jfltevzw: permissions: Add CDMA telephony permission
    And move jf-common inheritance to top.
    
    Change-Id: Ifafebd8dee8d282eb4d6a04d1d5e586dc53cd907

--- device.mk
+++ device.mk
@@ -13,10 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-## (2) Also get non-open-source specific aspects if available
+# Inherit from jf-common
+$(call inherit-product, device/samsung/jf-common/jf-common.mk)
+
+# Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/jfltevzw/jfltevzw-vendor.mk)
 
-# loki
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/samsung/jfltevzw/overlay
+
+# Loki
 PRODUCT_PACKAGES += \
     loki_tool \
     loki.sh \
@@ -26,11 +32,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/samsung/jfltevzw/releasetools/loki.sh:install/bin/loki.sh
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml
+
 # System Properties
 $(call inherit-product, device/samsung/jfltevzw/system_prop.mk)
-
-## device overlays
-DEVICE_PACKAGE_OVERLAYS += device/samsung/jfltevzw/overlay
-
-# Inherit from jf-common
-$(call inherit-product, device/samsung/jf-common/jf-common.mk)
