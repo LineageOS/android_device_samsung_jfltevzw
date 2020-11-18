9ebf8403e98c3cc84b68726a9d369510ab397033
Scott Warner <Tortel1210@gmail.com>
2020-11-18 12:25:53 -0500

    jfltevzw: releasetools: Use toybox to mount partitions
    Changes with the mount command in releasetools broke the old commands,
    so just call toybox when needed
    
    Change-Id: I5b2f88e5fb4da07fdcdd7a028125dd8c6cb854ff

--- releasetools/loki.sh
+++ releasetools/loki.sh
@@ -8,17 +8,38 @@
 # Run loki patch on boot.img for locked bootloaders, found in loki_bootloaders
 #
 
+LOSRECOVERY=/sbin/toybox
 egrep -q -f /system/etc/loki_bootloaders /proc/cmdline
+
 if [ $? -eq 0 ];then
   echo '[*] Locked bootloader version detected.'
   export C=/tmp/loki_tmpdir
   mkdir -p $C
   dd if=/dev/block/platform/msm_sdcc.1/by-name/aboot of=$C/aboot.img
+
+  # Mount parittions
+  if test -f "$LOSRECOVERY"; then
+      toybox mount /dev/block/bootdevice/by-name/system -t ext4 /mnt/system
+      toybox mount /dev/block/bootdevice/by-name/vendor -t ext4 /mnt/vendor
+  else
+      /tmp/toybox mount /dev/block/bootdevice/by-name/system -t ext4 /mnt/system
+      /tmp/toybox mount /dev/block/bootdevice/by-name/vendor -t ext4 /mnt/vendor
+  fi
+
   echo '[*] Patching boot.img to with loki.'
   /system/bin/loki_tool patch boot $C/aboot.img /tmp/boot.img $C/boot.lok || exit 1
   echo '[*] Flashing modified boot.img to device.'
   /system/bin/loki_tool flash boot $C/boot.lok || exit 1
   rm -rf $C
+
+  # Unmount partitions
+  if test -f "$LOSRECOVERY"; then
+      toybox umount /mnt/system
+      toybox umount /mnt/vendor
+  else
+      /tmp/toybox umount /mnt/system
+      /tmp/toybox umount /mnt/vendor
+  fi
 else
   echo '[*] Unlocked bootloader version detected.'
   echo '[*] Flashing unmodified boot.img to device.'
